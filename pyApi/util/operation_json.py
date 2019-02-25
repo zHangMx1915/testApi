1297dccdb767e7120bc930bde0c9b0e24d9b3c3f
zHangMx1915 <44599822+zHangMx1915@users.noreply.github.com>
2019-02-25 17:58:06 +0800

    Add files via upload

--- pyApi/util/operation_json.py
+++ pyApi/util/operation_json.py
@@ -15,10 +15,17 @@ class OpenJson:
 
     # 根据关键字获取数据
     def get_data(self, ids):
+        """
+        请求参数直接写在用例表格中时，写在{}中，判断如字符首尾为{}时，
+        将建字符串转换为dict格式后直接返回，否则再去json文件中读取参数
+        """
         if ids == None:
             return None
         else:
-            return self.data[ids]
+            if ids.startswith('{') and ids.endswith('}'):   # 判断是否以{}开头和结尾
+                return eval(ids)                            # 将str转换为dict格式
+            else:
+                return self.data[ids]
 
 
 # if __name__ == '__main__':
